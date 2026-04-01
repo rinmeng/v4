51314bf22f88975a41ec709a23e32b4ddea11fc6
rinmeng-gitbash <phurinmeng@gmail.com>
2026-03-31 23:25:16 -0700

    refactor: standardize import statements and add Tseng Photo project details

--- src/components/ProjectsFragment.tsx
+++ src/components/ProjectsFragment.tsx
@@ -1,5 +1,5 @@
-import { AspectRatio } from "@/components/ui/aspect-ratio";
-import { Button } from "@/components/ui/button";
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -7,49 +7,50 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { ArrowBigRightIcon, ArrowRight } from "lucide-react";
-import React from "react";
-import Link from "next/link";
+} from '@/components/ui/card';
+import { ArrowBigRightIcon, ArrowRight } from 'lucide-react';
+import React from 'react';
+import Link from 'next/link';
 
 // Import all logos
-import ViteLogo from "@/assets/logos/vite-svgrepo-com.svg";
-import ReactLogo from "@/assets/logos/react-svgrepo-com.svg";
-import TailwindCSSLogo from "@/assets/logos/tailwindcss-icon-svgrepo-com.svg";
-import ExpressLogo from "@/assets/logos/express-svgrepo-com.svg";
-import ShadCNUILogo from "@/assets/logos/shadcnui-white.svg";
-import DockerLogo from "@/assets/logos/docker-svgrepo-com.svg";
-import NodeJSLogo from "@/assets/logos/node-js-svgrepo-com.svg";
-import HTML5Logo from "@/assets/logos/html-5-svgrepo-com.svg";
-import CSS3Logo from "@/assets/logos/css-svgrepo-com.svg";
-import GitLogo from "@/assets/logos/git-svgrepo-com.svg";
-import PostgreSQLLogo from "@/assets/logos/postgresql-svgrepo-com.svg";
-import JavaScriptLogo from "@/assets/logos/javascript-svgrepo-com.svg";
-import TypeScriptLogo from "@/assets/logos/typescript-svgrepo-com.svg";
-import VercelLogo from "@/assets/logos/vercel-fill-white.svg";
-import SupabaseLogo from "@/assets/logos/supabase-seeklogo.svg";
-import JavaLogo from "@/assets/logos/java-svgrepo-com.svg";
-import AndroidStudioLogo from "@/assets/logos/androidstudio-svgrepo-com.svg";
-import FirebaseLogo from "@/assets/logos/firebase-svgrepo-com.svg";
+import ViteLogo from '@/assets/logos/vite-svgrepo-com.svg';
+import ReactLogo from '@/assets/logos/react-svgrepo-com.svg';
+import TailwindCSSLogo from '@/assets/logos/tailwindcss-icon-svgrepo-com.svg';
+import ExpressLogo from '@/assets/logos/express-svgrepo-com.svg';
+import ShadCNUILogo from '@/assets/logos/shadcnui-white.svg';
+import DockerLogo from '@/assets/logos/docker-svgrepo-com.svg';
+import NodeJSLogo from '@/assets/logos/node-js-svgrepo-com.svg';
+import HTML5Logo from '@/assets/logos/html-5-svgrepo-com.svg';
+import CSS3Logo from '@/assets/logos/css-svgrepo-com.svg';
+import GitLogo from '@/assets/logos/git-svgrepo-com.svg';
+import PostgreSQLLogo from '@/assets/logos/postgresql-svgrepo-com.svg';
+import JavaScriptLogo from '@/assets/logos/javascript-svgrepo-com.svg';
+import TypeScriptLogo from '@/assets/logos/typescript-svgrepo-com.svg';
+import VercelLogo from '@/assets/logos/vercel-fill-white.svg';
+import SupabaseLogo from '@/assets/logos/supabase-seeklogo.svg';
+import JavaLogo from '@/assets/logos/java-svgrepo-com.svg';
+import AndroidStudioLogo from '@/assets/logos/androidstudio-svgrepo-com.svg';
+import FirebaseLogo from '@/assets/logos/firebase-svgrepo-com.svg';
 // import PythonLogo from "@/assets/logos/python-svgrepo-com.svg";
-import GitHubLogo from "@/assets/logos/github-white.svg";
-import MySQLLogo from "@/assets/logos/mysql-svgrepo-com.svg";
-import CSharpLogo from "@/assets/logos/csharp-svgrepo-com.svg";
-import UnityLogo from "@/assets/logos/unity-svgrepo-com.svg";
-import NextJSLogo from "@/assets/logos/next-js-svgrepo-com.svg";
+import GitHubLogo from '@/assets/logos/github-white.svg';
+import MySQLLogo from '@/assets/logos/mysql-svgrepo-com.svg';
+import CSharpLogo from '@/assets/logos/csharp-svgrepo-com.svg';
+import UnityLogo from '@/assets/logos/unity-svgrepo-com.svg';
+import NextJSLogo from '@/assets/logos/next-js-svgrepo-com.svg';
 
 // import images
-import KDT from "@/assets/projects/KDT.png";
-import RM from "@/assets/projects/RM.png";
-import PC8TH from "@/assets/projects/PC8TH.png";
-import CTMS from "@/assets/projects/CTMS.png";
-import HIKELOWNA from "@/assets/projects/HIKELOWNA.png";
-import PHIL331 from "@/assets/projects/PHIL331.png";
-import COSC416 from "@/assets/projects/COSC416.png";
-import SEAC from "@/assets/projects/SEAC.png";
-import NSSS from "@/assets/projects/NSSS.png";
+import KDT from '@/assets/projects/KDT.png';
+import TsengPhoto from '@/assets/projects/tsengphoto.png';
+import RM from '@/assets/projects/RM.png';
+import PC8TH from '@/assets/projects/PC8TH.png';
+import CTMS from '@/assets/projects/CTMS.png';
+import HIKELOWNA from '@/assets/projects/HIKELOWNA.png';
+import PHIL331 from '@/assets/projects/PHIL331.png';
+import COSC416 from '@/assets/projects/COSC416.png';
+import SEAC from '@/assets/projects/SEAC.png';
+import NSSS from '@/assets/projects/NSSS.png';
 
-import Image, { StaticImageData } from "next/image";
+import Image, { StaticImageData } from 'next/image';
 
 interface ProjectFragmentProps {
   title: string;
@@ -62,14 +63,12 @@ interface ProjectFragmentProps {
 
 const projects = [
   {
-    title: "KPop Dance Team's Website",
-
-    imgSrc: KDT,
-    description: `I oversaw the Digital Producer role where I was in charge of their website development, maintenance. 
-      I also developed a custom CMS for them to manage their website content such as links, positions, and sponsors.
-      Multiple iterations of the website were created, now it is in its v4 stage, built with NextJS and shadcn/ui with optimized SEO and performance.`,
-    link: "https://kdtsuo.vercel.app",
-    github_link: "https://github.com/kdtsuo/v4",
+    title: 'Tseng Photo',
+    imgSrc: TsengPhoto,
+    description: `Tseng Photo is a professional photography portfolio website for showcasing works and services.
+     Built with the same robust tech stack as KDT, it features a modern, performant, and SEO-optimized design.`,
+    link: 'https://tsengphoto.ca',
+    github_link: 'https://github.com/rinmeng/tsengphoto',
     stacks: [
       HTML5Logo,
       CSS3Logo,
@@ -87,14 +86,39 @@ const projects = [
     ],
   },
   {
-    title: "next-shadcn-supabase-starter",
+    title: "KPop Dance Team's Website",
+
+    imgSrc: KDT,
+    description: `I oversaw the Digital Producer role where I was in charge of their website development, maintenance. 
+      I also developed a custom CMS for them to manage their website content such as links, positions, and sponsors.
+      Multiple iterations of the website were created, now it is in its v4 stage, built with NextJS and shadcn/ui with optimized SEO and performance.`,
+    link: 'https://kdtsuo.vercel.app',
+    github_link: 'https://github.com/kdtsuo/v4',
+    stacks: [
+      HTML5Logo,
+      CSS3Logo,
+      JavaScriptLogo,
+      TypeScriptLogo,
+      NodeJSLogo,
+      NextJSLogo,
+      ReactLogo,
+      TypeScriptLogo,
+      TailwindCSSLogo,
+      ShadCNUILogo,
+      VercelLogo,
+      SupabaseLogo,
+      PostgreSQLLogo,
+    ],
+  },
+  {
+    title: 'next-shadcn-supabase-starter',
     imgSrc: NSSS,
     description: `A comprehensive starter template with everything you need to build modern web applications by me. 
     Features enterprise-grade security with Supabase Auth, email verification, protected routes, and middleware proxy. 
     Built with cutting-edge technologies including Next.js 15 App Router, TypeScript, Tailwind CSS, and Shadcn UI. 
     Includes scalable architecture with API versioning, server components, and modular design for optimal developer experience.`,
-    link: "https://template.rinm.dev",
-    github_link: "https://github.com/rinmeng/next-shadcn-supabase-starter",
+    link: 'https://template.rinm.dev',
+    github_link: 'https://github.com/rinmeng/next-shadcn-supabase-starter',
     stacks: [
       NextJSLogo,
       ReactLogo,
@@ -118,8 +142,8 @@ const projects = [
     description: `Developed a now, production-ready website for the South East Asian Club (SEAC) 
     that highlights the club’s mission, events, and merchandise. As the lead Web Developer, 
     I designed and built a custom CMS to allow executives to easily manage and update content.`,
-    link: "https://seacsuo.vercel.app",
-    github_link: "https://github.com/seacsuo/v2",
+    link: 'https://seacsuo.vercel.app',
+    github_link: 'https://github.com/seacsuo/v2',
     stacks: [
       HTML5Logo,
       CSS3Logo,
@@ -136,7 +160,7 @@ const projects = [
     ],
   },
   {
-    title: "Collaborative Task Management System (CTMS)",
+    title: 'Collaborative Task Management System (CTMS)',
     imgSrc: CTMS,
     stacks: [
       HTML5Logo,
@@ -157,10 +181,10 @@ const projects = [
     developing both the frontend and backend of the system.
     This was my first full exposure to full-stack development and RESTful APIs.
     `,
-    github_link: "https://github.com/rinmeng/NodeNinjas",
+    github_link: 'https://github.com/rinmeng/NodeNinjas',
   },
   {
-    title: "3 Big Booms",
+    title: '3 Big Booms',
     imgSrc: COSC416,
     stacks: [UnityLogo, CSharpLogo],
     description: `This is a game development project in Unity that 
@@ -168,11 +192,11 @@ const projects = [
     "twist" theme, where we transformed a Bomberman game into a 2D platformer game.
     I was in charge of the game design and development, where I designed the game's mechanics,
     such as power-ups, enemy AI, and weapons.`,
-    link: "https://stewdio.itch.io/3-big-booms",
-    github_link: "https://github.com/eagno/cosc416-Project",
+    link: 'https://stewdio.itch.io/3-big-booms',
+    github_link: 'https://github.com/eagno/cosc416-Project',
   },
   {
-    title: "PHIL331 Project DCE Surveys",
+    title: 'PHIL331 Project DCE Surveys',
     imgSrc: PHIL331,
     stacks: [
       HTML5Logo,
@@ -192,12 +216,12 @@ const projects = [
     is functional and intuitive so that the users can easily take the surveys, and 
     the data is collected properly. The main skills gained here are mainly using graphs
     from shadcn/ui and Supabase's Auth Policies configuration.`,
-    link: "https://phil331.vercel.app",
-    github_link: "https://github.com/rinmeng/phil331",
+    link: 'https://phil331.vercel.app',
+    github_link: 'https://github.com/rinmeng/phil331',
   },
 
   {
-    title: "Personal Website",
+    title: 'Personal Website',
     imgSrc: RM,
     stacks: [
       HTML5Logo,
@@ -216,12 +240,12 @@ const projects = [
     description: `There are many stages to my personal website development. This website follows similar iterations to KPop Dance Team's website, 
     where I went from a simple HTML/CSS website, to NodeJS TailwindCSS, to ReactJS + TailwindCSS, to Vite + ReactTS + shadcn/ui,
     and now to NextJS + ReactTS + shadcn/ui.`,
-    link: "https://rinmeng.vercel.app",
-    github_link: "https://github.com/rinmeng/v4-rinmeng",
+    link: 'https://rinmeng.vercel.app',
+    github_link: 'https://github.com/rinmeng/v4-rinmeng',
   },
 
   {
-    title: "PC8TH",
+    title: 'PC8TH',
     imgSrc: PC8TH,
     stacks: [
       HTML5Logo,
@@ -236,16 +260,16 @@ const projects = [
     description: `PC8th is an PC parts e-commerce website that me and my partner developed for a course project. 
     It is my first exposure to backend development and I learned an immense amount of how backend system works. 
     This project was honorably mentioned by the professor of the course.`,
-    github_link: "https://github.com/rinmeng/pc8th",
+    github_link: 'https://github.com/rinmeng/pc8th',
   },
   {
-    title: "hikelowna",
+    title: 'hikelowna',
     imgSrc: HIKELOWNA,
     stacks: [JavaLogo, AndroidStudioLogo, FirebaseLogo],
     description: `hikelowna is a hiking trail management system that I developed for a course project with my group members. 
     This was my first exposure to Android development and I learned a lot about how Android apps are developed 
     and how they interact with the backend.`,
-    github_link: "https://github.com/rinmeng/hikelowna",
+    github_link: 'https://github.com/rinmeng/hikelowna',
   },
 ];
 
@@ -268,7 +292,7 @@ const ProjectFragment: React.FC<ProjectFragmentProps> = ({
               alt={title}
               width={1920}
               height={1080}
-              style={{ objectFit: "cover" }}
+              style={{ objectFit: 'cover' }}
             />
           </AspectRatio>
         </CardContent>
@@ -303,7 +327,7 @@ const ProjectFragment: React.FC<ProjectFragmentProps> = ({
         <CardFooter className='flex flex-wrap justify-end gap-4'>
           {github_link && (
             <Button
-              variant={"secondary"}
+              variant={'secondary'}
               size='lg'
               className='w-full md:w-auto'
               onClick={() => window.open(github_link)}
