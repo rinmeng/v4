93330e2ff0dfc8d8439c32a9e48947981cf0b56d
rinmeng-gitbash <phurinmeng@gmail.com>
2026-04-02 02:06:48 -0700

    refactor: simplify Loader2 component usage in Contacts page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -243,7 +243,7 @@ export default function Contacts() {
                       >
                         {isSubmitting ? (
                           <>
-                            <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+                            <Loader2 className='animate-spin' />
                             Sending...
                           </>
                         ) : (
